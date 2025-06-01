6a74d8dbde81b768844d18ff2fe37ad0f00ad7b6
Woldemst <adlerrein@gmail.com>
2025-06-01 23:36:57 +0200

    modified the getHistoric data and show more detail view in the log

--- bot.js
+++ bot.js
@@ -50,8 +50,8 @@ async function run() {
     // Test historical data  function
     // try {
       // Change this line
-      // const m1Data = await getHistorical("OIL_CRUDE", "m1", 50, "2025-05-25T15:09:47", "2025-05-26T15:10:05");
-      await getMarkets();
+      const m1Data = await getHistorical("EURTUSD", "m1", 50, "2025-05-25T15:09:47", "2025-05-26T15:10:05");
+      // await getMarkets();
     //   console.log(`Successfully fetched historical data for EUR_USD: ${m1Data.prices.length} candles`);
     // } catch (error) {
     //   console.error("Error testing historical data:", error.message);
